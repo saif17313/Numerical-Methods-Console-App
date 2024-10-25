a3d3bed42ce30ac01fc4f6d52fb2b21cfc95c203
Ruhan37 <139493936+Ruhan37@users.noreply.github.com>
2024-10-25 20:03:31 +0600

    Update Numerical Methods Console App.cpp

--- Numerical Methods Console App.cpp
+++ Numerical Methods Console App.cpp
@@ -8,7 +8,9 @@ int main()
     int w = 80; 
     string message = "\033[1mKUET Numerical Methods Console Application\033[0m"; 
     cout << setw((w + message.length() - 9) / 2) << message << endl;
-
+    int s;
+    cout<<"Enter an option to select:"<<endl;
+    cout<<"For \033[1mLinear Equation\033[0m select 1"<<endl;
     cout<<""<<endl;
     return 0;
 }
