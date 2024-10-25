c72650c453b341fb3d66129f85c195d5c272e039
Abdullah Al Saif <141681612+saif17313@users.noreply.github.com>
2024-10-25 20:25:16 +0600

    Update Numerical Methods Console App.cpp

--- Numerical Methods Console App.cpp
+++ Numerical Methods Console App.cpp
@@ -9,8 +9,11 @@ int main()
     string message = "\033[1mKUET Numerical Methods Console Application\033[0m"; 
     cout << setw((w + message.length() - 9) / 2) << message << endl;
     int s;
-    cout<<"Enter an option to select:"<<endl;
-    cout<<"For \033[1mLinear Equation\033[0m select 1"<<endl;
-    cout<<""<<endl;
+    cout<<"\033[1;32m-->\033[0m For \033[1;33mSolution of Linear Equations\033[0m select \033[1;33m1\033[0m"<<endl;
+    cout<<"\033[1;32m-->\033[0m For \033[1;33mSolution of Non-Linear Equations\033[0m select \033[1;33m2\033[0m"<<endl;
+    cout<<"\033[1;32m-->\033[0m For \033[1;33mSolution of Differential Equations\033[0m select \033[1;33m3\033[0m"<<endl;
+    cout<<"\033[1;32m-->\033[0m For \033[1;33mMatrix Inversion\033[0m select \033[1;33m4\033[0m\n"<<endl;
+    cout<<"\033[1;36m# Enter an option to select:\033[0m"<<endl;
+    cin>>s;
     return 0;
 }
