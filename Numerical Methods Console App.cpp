f1f72c4b50a792faed93ee13660eaac42e0aadfe
dip-shekhor <dips8137@gmail.com>
2024-10-26 00:23:46 +0600

    guass -seidel uploaded

--- Numerical Methods Console App.cpp
+++ Numerical Methods Console App.cpp
@@ -1,7 +1,54 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void guass_seidel()
+{
+    cout << " Enter No of variables: ";
+    int var; 
+    cin >> var;
+    float matx [var][var+ 1];
+    for(int i=0;i<var;i++)
+    {
+        for(int j=0;j<=var;j++)
+        {
+            cin>>matx[i][j];
 
+        }
+    }
+    int itr;
+    cout<<"Enter  the number of iterations: ";
+    cin>>itr;
+    cout<<endl;
+    vector<float>curV(var,0.0);
+    vector<float>prev(var,0.0);
+    vector<float>error(var,0.0);
+    int count=0;
+    while(itr--)
+    {
+      for(int i=0;i<var;i++)
+      { 
+        float sum=matx[i][var];
+        for(int j=0;j<var;j++)
+        {
+           
+            if(j!=i)    
+            {
+                sum-=matx[i][j]*curV[j];
+            }
+
+        }
+        curV[i]=sum/matx[i][i];
+        curV[i]=fabs(curV[i]);
+        cout<<curV[i]<<" ";
+        error[i]=fabs(curV[i]-prev[i]);
+        prev[i]=curV[i];
+      }
+      cout<<endl;
+      if(error[0]<0.00001)break;
+      count++;
+    }
+    cout<<"Total iterations: "<<count;
+}
 
 int main()
 {
@@ -16,6 +63,6 @@ int main()
     cout<<"\033[1;36m# Enter an option to select:\033[0m"<<endl;
     cin>>s;
 
-    
+
     return 0;
 }
