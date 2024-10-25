8763984969c8d6f26f4f31f89b119baa5e6474c3
Abdullah Al Saif <141681612+saif17313@users.noreply.github.com>
2024-10-25 19:52:45 +0600

    Add files via upload

---
+++ Numerical Methods Console App.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main()
+{
+    int w = 80; 
+    string message = "\033[1mKUET Numerical Methods Console Application\033[0m"; 
+    cout << setw((w + message.length() - 9) / 2) << message << endl;
+
+    cout<<""<<endl;
+    return 0;
+}
