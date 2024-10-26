42ffbcb900029f94b01f8a8ed265423dcc6c10a7
Abdullah Al Saif <141681612+saif17313@users.noreply.github.com>
2024-10-26 15:18:24 +0600

    Update Numerical Methods Console App.cpp

--- Numerical Methods Console App.cpp
+++ Numerical Methods Console App.cpp
@@ -360,6 +360,70 @@ void gaussJordanElimination(int n) {
     }
 }
 
+void inversion(int n) {
+    vector<vector<double>>matrix(n,vector<double>(n+n));
+        cout<<"Enter elements \033[1;35ma1, a2, ..., an\033[0m\n";
+
+    for(int i=0;i<n;i++)
+    {
+        for (int j=0;j<n;j++)
+        {
+            cin>>matrix[i][j];
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+         for (int j=n;j<n+n;j++)
+        {
+            if((j-i)==n)
+            matrix[i][j]=1;
+            else
+            matrix[i][j]=0;
+        }
+    }
+
+    for(int i=0;i<n;i++)
+    {
+        int max=i;
+        for (int k=i+1;k<n;k++)
+        {
+            if(fabs(matrix[k][i])>fabs(matrix[max][i]))
+            {
+                max=k;
+            }
+        }
+        swap(matrix[i], matrix[max]);
+        if (fabs(matrix[i][i]) < 1e-10) {
+            cout<<"\033[1;31mNo unique solution exists\033[0m"<<endl;
+            return;
+        }
+        double pivot=matrix[i][i];
+        for (int k=0;k<n+n;k++) {
+            matrix[i][k]/= pivot;
+        }
+
+        for (int j=0;j<n;j++)
+        {
+            if (j!=i) {
+                double factor=matrix[j][i];
+                for (int k=0; k<n+n;k++) {
+                    matrix[j][k]-=factor*matrix[i][k];
+                }
+            }
+        }
+    }
+
+    cout << "\033[1;35mThe Inverse Matrix is :\033[0m"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=n;j<n+n;j++)
+        {
+            cout<<matrix[i][j]<<"  ";
+        }
+        cout<<endl;
+    }
+}
+
 
 ld secant()
 {
@@ -387,7 +451,6 @@ while (true)
      return x3;
 }
 
-
 int main()
 {
     int w = 80; 
@@ -397,7 +460,9 @@ int main()
     string option="\033[1;32m-->\033[0m For \033[1;33mSolution of Linear Equations\033[0m enter \033[1;33m1\033[0m\n\033[1;32m-->\033[0m For \033[1;33mSolution of Non-Linear Equations\033[0m enter \033[1;33m2\033[0m\n\033[1;32m-->\033[0m For \033[1;33mSolution of Differential Equations\033[0m enter \033[1;33m3\033[0m\n\033[1;32m-->\033[0m For \033[1;33mMatrix Inversion\033[0m enter \033[1;33m4\033[0m\n\033[1;32m-->\033[0m To  \033[1;31mEXIT\033[0m enter \033[1;31m-1\033[0m\n";
     string choose="\033[1;36m# Enter an option to select :\033[0m\n";
     string nonlinear="\033[1;32mMethods for solving non-linear equations :\033[0m\n\033[1;32m-->\033[0m For \033[1;33mBi-section method\033[0m enter \033[1;33m1\033[0m\n\033[1;32m-->\033[0m For \033[1;33mFalse position method\033[0m enter \033[1;33m2\033[0m\n\033[1;32m-->\033[0m For \033[1;33mSecant method\033[0m enter \033[1;33m3\033[0m\n\033[1;32m-->\033[0m For \033[1;33mNewton-Raphson method\033[0m enter \033[1;33m4\033[0m\n\033[1;32m-->\033[0m For \033[1;33mMain Menu\033[0m enter \033[1;33m0\033[0m\n\033[1;32m-->\033[0m To  \033[1;31mEXIT\033[0m enter \033[1;31m-1\033[0m\n";
-    cout<<"\n";
+    string inverseoption="\033[1;32m-->\033[0m For \033[1;33mInversion of Matrix\033[0m enter \033[1;33m1\033[0m\n\033[1;32m-->\033[0m For \033[1;33mMain Menu\033[0m enter \033[1;33m0\033[0m\n\033[1;32m-->\033[0m To  \033[1;31mEXIT\033[0m enter \033[1;31m-1\033[0m\n";
+    string rangeoption="\033[1;32m-->\033[0m For \033[1;33mRange-Kutta Method\033[0m enter \033[1;33m1\033[0m\n\033[1;32m-->\033[0m For \033[1;33mMain Menu\033[0m enter \033[1;33m0\033[0m\n\033[1;32m-->\033[0m To  \033[1;31mEXIT\033[0m enter \033[1;31m-1\033[0m\n";
+    string again="\033[1mDo you want to continue ?\033[0m\nPress \033[1;35m1\033[0m for \033[1;35mYES\033[0m\nPress \033[1;31mAny Integer\033[0m for \033[1;31mNO\033[0m :\n";
     cout << setw((w + message.length() - 9) / 2) << message << endl;
     cout<<"\n";
     int s;
@@ -416,7 +481,7 @@ int main()
          cin>>l1;
          if(l1!=0 && l1!=-1)
          {
-         cout<<"Enter variable no.:\n";
+         cout<<"Enter \033[1;35mvariable no.\033[0m:\n";
          cin>>n;
          }
          switch(l1)
@@ -428,18 +493,48 @@ int main()
               return 0;
             case 1:
               jacobian(n);
+              cout<<again;
+              cin>>chk;
+              if(chk==1)
+                goto main;
+              else
+                return 0;
               break;
             case 2:
               gauss_seidel(n);
+              cout<<again;
+              cin>>chk;
+              if(chk==1)
+                goto main;
+              else
+                return 0;
               break;
             case 3:
               gaussElimination(n);
+              cout<<again;
+              cin>>chk;
+              if(chk==1)
+                goto main;
+              else
+                return 0;
               break;
             case 4:
               gaussJordanElimination(n);
+              cout<<again;
+              cin>>chk;
+              if(chk==1)
+                goto main;
+              else
+                return 0;
               break;
             case 5:
              //lu
+             cout<<again;
+              cin>>chk;
+              if(chk==1)
+                goto main;
+              else
+                return 0;
               break;
             default:
               cout<<error;
@@ -478,7 +573,7 @@ int main()
            }
            else
            {
-              cout<<"\033[1mEnter \033[1;35ma, b, c\033[0m in the \033[1;35m asin(x) + bcos(x) + ctan(x) +d\033[0m :\n";
+              cout<<"\033[1mEnter \033[1;35ma, b, c, d\033[0m in the \033[1;35m asin(x) + bcos(x) + ctan(x) +d\033[0m :\n";
               cin>>a>>b>>c>>d;
            }
         }
@@ -490,29 +585,118 @@ int main()
               return 0;
             case 1:
               bisection();
+              cout<<again;
+              cin>>chk;
+              if(chk==1)
+                goto main;
+              else
+                return 0;
               break;
             case 2:
               false_position();
+              cout<<again;
+              cin>>chk;
+              if(chk==1)
+                goto main;
+              else
+                return 0;
               break;
             case 3:
-              //secant();
+              secant();
+              cout<<again;
+              cin>>chk;
+              if(chk==1)
+                goto main;
+              else
+                return 0;
               break;
             case 4:
-              newton_raphsonAlgebric();
-              break;
-            case 5:
+              //newton_raphsonAlgebric();
+              cout<<again;
+              cin>>chk;
+              if(chk==1)
+                goto main;
+              else
+                return 0;
               break;
             default:
               cout<<error;
               goto non_linear;  
         }
         break;
+        case 3:
+        range:
+          int l4,n3;
+          cout<<rangeoption;
+          cout<<choose;
+          cin>>l4;
+          if(l4!=0 && l4!=-1)
+          {
+
+          }
+          switch(l4){
+            case 1:
+              //range_kutta();
+              cout<<again;
+              cin>>chk;
+              if(chk==1)
+                goto main;
+              else
+                return 0;
+              break;
+            case 0:
+               goto main;
+               break;
+            case -1:
+               return 0;
+            default:
+              cout<<error;
+              goto inv;
+              break;
+              
+         }
+         break;
+        case 4:
+        inv:
+         int l3,n2;
+         cout<<inverseoption;
+         cout<<choose;
+         cin>>l3;
+         if(l3!=0 && l3!=-1)
+         {
+         cout<<"Enter \033[1;35mn\033[0m for \033[1;35m(n*n)\033[0m matrix:\n";
+         cin>>n2;
+         }
+         else{
+            cout<<error;
+            goto inv;
+         }
+         switch(l3){
+            case 1:
+              inversion(n2);
+              cout<<again;
+              cin>>chk;
+              if(chk==1)
+                goto main;
+              else
+                return 0;
+              break;
+            case 0:
+               goto main;
+               break;
+            case -1:
+               return 0;
+            default:
+              cout<<error;
+              goto inv;
+              break;
+              
+         }
+         break;
     default:
           cout<<error;
           goto main;
           break;
-
-         
     }
     return 0;
 }
